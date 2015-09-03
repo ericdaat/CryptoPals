dd474765bf37d2c60557f7f0aa74bcd7a70656f4
Eric Daoud <ericda@outlook.com>
2015-09-03 18:42:27 +0200

    First sight of a solution
    Trying multiple matches and showing results

--- Python/set1_ex3.py
+++ Python/set1_ex3.py
@@ -1,11 +1,22 @@
 from collections import Counter
 
-input_string = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"
-c = Counter(input_string).most_common()
+def xor_strings(xs, ys):
+    return "".join(chr(ord(x) ^ ord(y)) for x, y in zip(xs, ys))
 
-for i in range(0,len(c)):
-	print "%c: %i%%" %(c[i][0],100*c[i][1]/len(input_string))
+hex_string = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"
+
+decoded_string = hex_string.decode("hex")
+frequency_analysis = Counter(decoded_string).most_common()
 
 
+for i in range(0,len(frequency_analysis)):
+	# printing frequency analysis
+	#print "%c: %i%%" %(frequency_analysis[i][0],100*frequency_analysis[i][1]/len(decoded_string))
 
+	#trying various keys
+	key = xor_strings(frequency_analysis[i][0],"e") #xoring most frequents letters in string against most frequent letter in english : 'e'
+	message = "" #storing the temporary message
+	for j in range (0,len(decoded_string)): #decoding with a key and storing the result in message string
+		message += xor_strings(decoded_string[j],key)
+	print "try %i :using key %s : %s" %(i,key,message) #printing key and message result
 
