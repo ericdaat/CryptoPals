00e6a716003460e56392fddcea9445d22e0a3379
Eric Daoud <ericda@outlook.com>
2015-11-02 13:00:35 +0100

    improved algorithm 6

--- Python/set1_ex6.py
+++ Python/set1_ex6.py
@@ -17,32 +17,30 @@ def hamming_distance(string_1,string_2):
 
 	return hamming_distance
 
-
 text = urllib2.urlopen("http://cryptopals.com/static/challenge-data/6.txt")
 text_string = text.read()
 
-keysize_array = []
-normalized_hamming_distance_array = []
+keysize = 0
+norm_hamming_distance = 1000
 
-for keysize in range(2,40):
-	part1 = text_string[0:keysize]
-	part2 = text_string[keysize:2*keysize]
+for temporary_keysize in range(2,40):
+	part1 = text_string[0:temporary_keysize]
+	part2 = text_string[temporary_keysize:2*temporary_keysize]
 
-	normalized_hamming_distance = hamming_distance(part1,part2) / float(keysize)
+	temp_norm_hamming_dist = hamming_distance(part1,part2) / float(temporary_keysize)
 
-	keysize_array.append(keysize)
-	normalized_hamming_distance_array.append(normalized_hamming_distance)
+	if temp_norm_hamming_dist < norm_hamming_distance:
+		norm_hamming_distance = temp_norm_hamming_dist
+		keysize = temporary_keysize
 
-normalized_hamming_distance_array, keysize_array = zip(*sorted(zip(normalized_hamming_distance_array, keysize_array)))
-
-
-best_key = keysize_array[0]
-
-a = map(''.join, zip(*[iter(text_string)]*best_key))
+a = map(''.join, zip(*[iter(text_string)]*keysize))
 transposed_a = []
 
-for j in range(best_key):
+for j in range(keysize):
 	transposed_a.append('')
 	for i in range(len(a)):
 		char = a[i][j]
 		transposed_a[j]+=char
+
+
+
